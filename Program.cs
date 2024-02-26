fba62975e3af9133313ce55e24c9e75c99caad75
Андрей Рашев <ar42698937@edu.mon.bg>
2024-02-26 19:49:30 +0200

    26/02/2024 19:50PM

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using BulkyWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace BulkyWeb
 {
     public class Program
@@ -8,6 +11,8 @@ namespace BulkyWeb
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
